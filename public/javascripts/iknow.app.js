fb8c6cdccbfcab13db6e911be5f7b5e8e15cf7ea
Dmitriy Kashin <wild.mephist@gmail.com>
2012-09-09 23:08:30 +0400

    Добавлено дерево событий

--- public/javascripts/iknow.app.js
+++ public/javascripts/iknow.app.js
@@ -5,18 +5,36 @@ $(function() {
 		$( "#metrics-denide" ).buttonset();
 		$( "#shows-selection" ).buttonset();
 		$( "#accordion" ).accordion();
+       
+        
 
 		
 			
           
           
-		var line = new RGraph.Line('cvs', [84,-5,-30,-26,-45,12,84,73,60]);
-        line.Set('chart.labels', ['Felicity','Gary','Neil','Jay','Helga','Ray','Kev','Luis','Pete']);
-        line.Set('chart.colors', ['white','green']);
+		
+          
+       
+       
+  
+        var first_metric = [1.5,2.5,2.1,1.3,1.9,2.1,1.1];
+        var second_metric = [2,3,4,5,2.9,3.1,2.4];
+        var third_metric =  [3.4,5,4.2,1.2,3.4,2.8];
+
+        function drawing(first_metric, second_metric, third_metric) {
+
+
+         var line = new RGraph.Line('cvs', first_metric, second_metric, third_metric);
+        line.Set('chart.linewidth', 3);
+        line.Set('chart.colors', ['black']);
+        line.Set('chart.ymax', 10);
+        line.Set('chart.tickmarks', 'endcircle');
+        line.Set('chart.tooltips', ['Charles','Rick','Huey','Pob','Kevin','Louis','John']);
+        line.Set('chart.colors', ['white','green','blue']);
         line.Set('chart.background.grid.color', 'white');
         line.Set('chart.background.barcolor1', 'orange');
-        line.Set('chart.text.color', 'white');
         line.Set('chart.background.barcolor2', 'orange');
+        line.Set('chart.text.color', 'white');       
         line.Set('chart.fillstyle', ['orange']);
         line.Set('chart.shadow', true);
         line.Set('chart.shadow.offsetx', 1);
@@ -26,6 +44,51 @@ $(function() {
         line.Set('chart.xaxispos', 'center');
         line.Set('chart.hmargin', 5);
         RGraph.Effects.Line.jQuery.Trace(line);
+
+
+
+        }
+
+        drawing(first_metric,second_metric,third_metric);
+
+      
+
+         $("#m-denide-2").click(function () {
+
+            if($("#m-denide-2").is(":checked")) {
+
+                
+                drawing(first_metric,null,third_metric);
+                second_metric=null;
+
+
+            } else 
+             {
+                second_metric=[2,3,4,5,2.9,3.1,2.4];
+                drawing(first_metric,second_metric,third_metric);
+                
+             }
+   
+           });  
+
+         $("#m-denide-3").click(function () {
+
+            if($("#m-denide-3").is(":checked")) {
+
+               
+                 drawing(first_metric,second_metric,null);
+                 third_metric=null;
+               
+
+
+            } else {
+                third_metric =  [3.4,5,4.2,1.2,3.4,2.8];
+                drawing(first_metric,second_metric,third_metric);
+            }
+   
+           });
+
+
     
 		
 	
@@ -34,3 +97,8 @@ $(function() {
 		
 	});
 
+
+            $(document).ready(function(){
+                $("#tree").Tree();
+            });
+  
